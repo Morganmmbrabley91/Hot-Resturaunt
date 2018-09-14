828d57f9f69751d690bde548a6746d01e7eeea31
Morganmmbrabley91 <morganmmbradley91@gmailcom>
2018-09-14 13:53:58 -0400

    condensed the files

--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -1,28 +1,20 @@
 var tables = require("../data/table-data.js");
 var waitlist = require("../data/waitlist-data.js");
-
 module.exports = function(app) {
-
 	app.get("/api/tables", function(req, res) {
 		res.json(tables);
 	});
-
 	app.get("/api/waitlist", function(req, res) {
 		res.json(waitlist);
 	});
-
 	app.post("/api/clear", function() {
 		tables = [];
 		waitlist = [];
-
 		console.log(tables);
 	});
-
 	app.post("/api/tables", function(req, res) {
   		var newTable = req.body;
-
  		 console.log(newTable);
-
   		if (tables.length < 5) {
 			tables.push(newTable);
 			res.json(true);
@@ -30,7 +22,5 @@ module.exports = function(app) {
 			waitlist.push(newTable);
 			res.json(false);
 		}
-
-  	
 	});
 };
--- app/routing/html-routes.js
+++ app/routing/html-routes.js
@@ -1,20 +1,14 @@
 var path = require("path");
-
 module.exports = function(app) {
-
 	app.get("/", function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/index.html"));
 	});
-
 	app.get("/tables", function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/tables.html"));
 	});
-
 	app.get("/reserve", function(req, res) {
 		res.sendFile(path.join(__dirname, "/../public/reservation.html"));
 	});
-
-
 	app.use(function (req, res){
 		res.sendFile(path.join(__dirname + '/../public/index.html'))
 	});
